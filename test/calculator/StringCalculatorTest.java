b4159e9cfb5451ec8efda98fc0dc8923fbd37941
A-Shchehlov <lov.sheg1ov@gmail.com>
2022-06-16 12:37:55 +0300

    STEP-4

--- test/calculator/StringCalculatorTest.java
+++ test/calculator/StringCalculatorTest.java
@@ -11,38 +11,50 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void step1TestStringWithOneDigit(){
+    public void step1TestStringWithOneDigit() {
         String nums = "1";
         Assert.assertEquals(1, StringCalculator.add(nums));
     }
 
     @Test
-    public void step1TestStringWithTwoDigits(){
+    public void step1TestStringWithTwoDigits() {
         String nums = "1, 2";
         Assert.assertEquals(3, StringCalculator.add(nums));
     }
 
     @Test
-    public void step2TestStringWithTenSingleDigitNumbers(){
+    public void step2TestStringWithTenSingleDigitNumbers() {
         String nums = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
         Assert.assertEquals(45, StringCalculator.add(nums));
     }
 
     @Test
-    public void step2TestStringWithTenTwoDigitNumbers(){
+    public void step2TestStringWithTenTwoDigitNumbers() {
         String nums = "10, 11, 12, 13, 14, 15, 16, 17, 18, 19";
         Assert.assertEquals(145, StringCalculator.add(nums));
     }
 
     @Test
-    public void step3TestStringWithNumbersDelimitedByNewLine1(){
+    public void step3TestStringWithNumbersDelimitedByNewLine1() {
         String nums = "1\\n2,3";
         Assert.assertEquals(6, StringCalculator.add(nums));
     }
 
     @Test
-    public void step3TestStringWithNumbersDelimitedByNewLine2(){
+    public void step3TestStringWithNumbersDelimitedByNewLine2() {
         String nums = "1\\n2,3\\n4\\n\\n5";
         Assert.assertEquals(15, StringCalculator.add(nums));
+    }
+
+    @Test
+    public void step4TestStringWithNumbersWithDifferentDelimiters() {
+        String nums = "//;\\n1;2";
+        Assert.assertEquals(3, StringCalculator.add(nums));
+    }
+
+    @Test
+    public void step4TestStringWithNumbersAndOtherSymbols() {
+        String nums = "1**as 2//>3,n45";
+        Assert.assertEquals(51, StringCalculator.add(nums));
     }
 }
